13940549fbfb71b93148b007553b4c836bb123fe
Joshua Harms <joshua@nozzlegear.com>
2018-09-01 16:19:43 -0500

    Adjust layout via prettier

--- src/components/html_layout.tsx
+++ src/components/html_layout.tsx
@@ -16,42 +16,30 @@ export function HtmlLayout({ children }: React.Props<any>) {
                     name="viewport"
                 />
                 {/* Favicons */}
-                <link rel="shortcut icon" href="/public/images/favicons/favicon.ico?v=3" type="image/x-icon" />
-                <link rel="apple-touch-icon" href="/public/images/favicons/apple-touch-icon.png?v=3" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="57x57"
-                    href="/public/images/favicons/apple-touch-icon-57x57.png?v=3"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="72x72"
-                    href="/public/images/favicons/apple-touch-icon-72x72.png?v=3"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="76x76"
-                    href="/public/images/favicons/apple-touch-icon-76x76.png?v=3"
-                />
+                <link rel="shortcut icon" href="/public/images/favicons/favicon.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" href="/public/images/favicons/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" sizes="57x57" href="/public/images/favicons/apple-touch-icon-57x57.png" />
+                <link rel="apple-touch-icon" sizes="72x72" href="/public/images/favicons/apple-touch-icon-72x72.png" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/public/images/favicons/apple-touch-icon-76x76.png" />
                 <link
                     rel="apple-touch-icon"
                     sizes="114x114"
-                    href="/public/images/favicons/apple-touch-icon-114x114.png?v=3"
+                    href="/public/images/favicons/apple-touch-icon-114x114.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="120x120"
-                    href="/public/images/favicons/apple-touch-icon-120x120.png?v=3"
+                    href="/public/images/favicons/apple-touch-icon-120x120.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="144x144"
-                    href="/public/images/favicons/apple-touch-icon-144x144.png?v=3"
+                    href="/public/images/favicons/apple-touch-icon-144x144.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="152x152"
-                    href="/public/images/favicons/apple-touch-icon-152x152.png?v=3"
+                    href="/public/images/favicons/apple-touch-icon-152x152.png"
                 />
 
                 {/* Bootstrap */}
